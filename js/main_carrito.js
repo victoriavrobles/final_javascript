8cb44c08afe1619f7c202e4574ad058fb515c08c
victoriavrobles <victoriavrobles@gmail.com>
2022-08-15 16:36:12 -0300

    css

--- js/main_carrito.js
+++ js/main_carrito.js
@@ -12,8 +12,9 @@ function ProductosCarrito() {
         let precio = producto.precio * producto.cantidad;
         contenido += `<tr>
         <td class="align-middle"><img src="${producto.imagen}" alt="${producto.nombre}" width="32"></td>
-        <td class="align-middle">${producto.nombre} x ${producto.cantidad}</td>
-        <td class="align-middle">$${precio}</td>
+        <td class="align-middle">${producto.nombre}</td>
+        <td class="align-middle">Cantidad: ${producto.cantidad}</td>
+        <td class="align-middle">Precio: $${precio}</td>
         <td class="text-end align-middle"><a href="#" class="btn btn-light" onclick="eliminarProducto(${producto.id})"><img src="assets/img/trash3.svg" width="18"></a></td>
         </tr>`;
         total += precio;
@@ -21,8 +22,9 @@ function ProductosCarrito() {
 
     contenido += `<tr>
     <td class="align-middle">&nbsp;</td>
-    <td class="align-middle"><b>Total a pagar:</b></td>
-    <td class="align-middle"><b>$${total}</b></td>
+    <td class="align-middle total-carrito"><b>Total a pagar:</b></td>
+    <td class="align-middle">&nbsp;</td>
+    <td class="align-middle total-carrito"><b>$${total}</b></td>
     <td class="align-middle text-end"><a href="./pago.html" class="btn btn-light">Finalizar compra</a></td>
     </tr>`
 
